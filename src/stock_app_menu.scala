d78496c8b9251637e4c4e5f4237caa5e41470ebe
Alexandra <alexandragoffova@gmail.com>
2022-12-12 21:59:54 +0000

    Added Portfolio Function

--- src/stock_app_menu.scala
+++ src/stock_app_menu.scala
@@ -1,117 +1,113 @@
 package example
 
+import scala.collection.immutable.Nil.{flatMap, head, headOption}
 import scala.collection.immutable.{ListMap, SortedMap}
+import scala.io.Source
 import scala.sys.SystemProperties.headless.key
 
-object HelloWorld extends App {
+object StockCalculator extends App {
+  //read stock data from file
+  val mapdata = readFile("data.txt")
 
-  val mapdata = Map(
-    "SK1" -> List(9, 7, 2, 0, 7, 3, 7, 9, 1, 2, 8, 1, 9, 6, 5, 3, 2, 2, 7, 2, 8, 5, 4, 5, 1, 6, 5, 2, 4, 1),
-    "SK2" -> List(0, 7, 6, 3, 3, 3, 1, 6, 9, 2, 9, 7, 8, 7, 3, 6, 3, 5, 5, 2, 9, 7, 3, 4, 6, 3, 4, 3, 4, 1),
-    "SK3" -> List(8, 7, 1, 8, 0, 5, 8, 3, 5, 9, 7, 5, 4, 7, 9, 8, 1, 4, 6, 5, 6, 6, 3, 6, 8, 8, 7, 4, 0, 6),
-    "SK4" -> List(2, 9, 5, 7, 0, 8, 6, 6, 7, 9, 0, 1, 3, 1, 6, 0, 0, 1, 3, 8, 5, 4, 0, 9, 7, 1, 4, 5, 2, 9),
-    "SK5" -> List(2, 6, 8, 0, 3, 5, 5, 2, 5, 9, 4, 5, 3, 5, 7, 8, 8, 2, 5, 9, 3, 8, 6, 7, 8, 7, 4, 1, 2, 3),
-    "SK6" -> List(2, 7, 5, 9, 1, 9, 8, 4, 1, 7, 3, 7, 0, 8, 4, 5, 9, 2, 4, 4, 8, 7, 9, 2, 2, 7, 9, 1, 6, 9),
-    "SK7" -> List(6, 9, 5, 0, 0, 0, 0, 5, 8, 3, 8, 7, 1, 9, 6, 1, 5, 3, 4, 7, 9, 5, 5, 9, 1, 4, 4, 0, 2, 0),
-    "SK8" -> List(2, 8, 8, 3, 1, 1, 0, 8, 5, 9, 0, 3, 1, 6, 8, 7, 9, 6, 7, 7, 0, 9, 5, 2, 5, 0, 2, 1, 8, 6),
-    "SK9" -> List(7, 1, 8, 8, 4, 4, 2, 2, 7, 4, 0, 6, 9, 5, 5, 4, 9, 1, 8, 6, 3, 4, 8, 2, 7, 9, 7, 2, 6, 6)
-  )
+  //Reads Data File From Data.txt
+  def readFile(filename: String): Map[String, Int] = {
+    // create buffer to build up map as we read each line
+    var mapBuffer: Map[String, Int] = Map()
+    try {
+      for (line <- Source.fromFile(filename).getLines()) {
+        // for each line
+        val splitline = line.split(",").map(_.trim).toList // split line at , and convert to List
 
+        // add element to map buffer
+        // splitline is line from file as List, e.g. List(Bayern Munich, 24)
+        // use head as key
+        // tail is a list, but need just the first (only in this case) element, so use head of tail and convert to int
+        mapBuffer = mapBuffer ++ Map(splitline.head -> splitline.tail.head.toInt)
+
+      }
+    } catch {
+      case ex: Exception => println("Sorry, an exception happened.")
+    }
+    mapBuffer
+  }
 
   def currentStockPrice(lmap: Map[String, List[Int]]) {
     val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
     mp foreach { case (key, value) => println("The current stock price of " + key + " is : " + value.last) }
   }
 
-  currentStockPrice(mapdata)
-
   def maxMinPrices(lmap: Map[String, List[Int]]) {
     val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
     mp foreach { case (key, value) => println(key + " : " + "Highest price : " + value.max + " Lowest Price : " + value.min) }
   }
 
-  maxMinPrices(mapdata)
-
   def findMedian(lmap: Map[String, List[Int]]): Unit = {
-    val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
-    mp foreach{
-      case (key, value)
-      => println(key + " => " + value.sorted.drop(value.length / 2).head)
-        if (value.sorted.size % 2 == 0) {
+    def median(lmap: Map[String, List[Int]]) {
+      val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
+      mp foreach {
+        case (key, value)
+        => if (value.sorted.size % 2 == 0) {
           println(value.sorted)
           val (up, down) = value.sorted.splitAt(value.size / 2)
           println(((up.last + down.head) / 2).toDouble)
         }
+        else {
+          println(key + " => " + value.sorted.drop(value.length / 2).head)
+        }
+      }
     }
+    median(lmap)
   }
 
-  findMedian(mapdata)
 
-  def getSymbolForWeek(lmap: Map[String, List[Int]]): Unit = {
-    val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
-    var xy = SortedMap[String, Int]()
+  /*def getSymbolForWeek(lmap: Map[String, Int]): Unit = {
+   val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
+   var xy = SortedMap[String, Int]()
     mp foreach {
       case (key, value)
-      => val week = value.drop(23)
-        val rateChange = (week.last - week.head)
-        xy = SortedMap(key -> rateChange) ++ xy
+      => xy = SortedMap(key -> (value.drop(23).last - value.drop(23).head)) ++ xy
     }
-    println("The stock that has shown largest rise over the last week is : " + (xy.maxBy(stock => stock._2)))
+    println(xy.maxBy(stock => stock._2))
   }
 
-  getSymbolForWeek(mapdata)
+  getSymbolForWeek(mapdata)*/
 
-  def getUsersChoice(lmap : Map[String, List[Int]]) = {
-    println("Please select two stocks from the following options :")
-    var x : Boolean = true
-    var optNum = 0
-    val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
-    mp foreach { case (key, value)
-    =>
-      optNum += 1
-      println(optNum + ". " + key)
-    }
+  /*def getInputUser(): Unit = {
     println("Select the first stock : ")
-    val firstStock = scala.io.StdIn.readLine().toUpperCase()
+    val x = scala.io.StdIn.readLine().toUpperCase()
     println("Second stock :")
-    val secStock = scala.io.StdIn.readLine().toUpperCase()
-    println("You have chosen options " + firstStock + " and " + secStock)
-    println("The average price for " + firstStock + " is : " + getAverage(mp(firstStock)))
-    println("The average price for " + secStock + " is : " + getAverage(mp(secStock)))
-    var larger,smaller = ""
-    if(firstStock > secStock){
-      larger = firstStock
-      smaller = secStock }
-    else{
-      larger = secStock
-      smaller = firstStock
+    val y = scala.io.StdIn.readLine().toUpperCase()
+    return x
+  }*/
+
+  def compareMeanValues(lmap: Map[String,Int]) = {
+    val mp = ListMap(lmap.toSeq.sortWith(_._1 < _._1): _*)
+    println("Select the first stock : ")
+    val x = scala.io.StdIn.readLine().toUpperCase()
+    println("Second stock :")
+    val y = scala.io.StdIn.readLine().toUpperCase()
+    println("The average price of " + x + " is " + getAverage(mp.get(x)) + " and " + y + " is " + getAverage(mp(y)))
+
+  }
+  compareMeanValues(mapdata)
+    def getAverage(list: List[Int]): Double = {
+      var average = list.foldLeft(0.0)(_ + _) / list.foldLeft(0)((r, c) => r + 1)
+      average = scala.BigDecimal(average).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+      return average
     }
-    var timesHigher = getAverage(mp(larger)) / getAverage(mp(smaller))
-    timesHigher = scala.BigDecimal(timesHigher).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-    println("This means that on average the value of " + larger + " is " + timesHigher + " times greater than " + smaller )
+
+    // Let the user input their own portfolio and add shared held.
+    def createPortfolio() = {
+      var xy = SortedMap[String, Int]()
+      println("Enter the stock symbol :")
+      val stockName = scala.io.StdIn.readLine().toUpperCase()
+      println("Enter the number of shares : ")
+      val shares = scala.io.StdIn.readInt()
+      val valList = mapdata(stockName)
+      println(valList)
+      val currentValue = shares * valList.last
+      println(currentValue)
+      xy = SortedMap(stockName -> currentValue) ++ xy
+    }
   }
 
-  def getAverage(list : List[Int]):Double = {
-    var average = list.foldLeft(0.0)(_+_) / list.foldLeft(0)((r,c)=>r+1)
-    average = scala.BigDecimal(average).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-    return average
-  }
-
-  // Let the user input their own portfolio and add shared held.
-  def createPortfolio() = {
-    var xy = SortedMap[String,Int]()
-    println("Enter the stock symbol :")
-    val stockName = scala.io.StdIn.readLine().toUpperCase()
-    println("Enter the number of shares : ")
-    val shares = scala.io.StdIn.readInt()
-    val valList = mapdata(stockName)
-    println(valList)
-    val currentValue = shares * valList.last
-    println(currentValue)
-    xy = SortedMap(stockName -> currentValue) ++ xy
-
-  }
-
-  createPortfolio()
-
-}
